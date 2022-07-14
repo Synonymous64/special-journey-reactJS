5883f52f7159cd776918ffbacee1f6f59d1f74a9
Synonymous64 <prajwalurkude007@gmail.com>
2022-07-14 23:01:30 +0530

    NavBar components

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
 export default class App extends Component {
   //* Class data member
   a = "by Prajwal :)";
   render() {
-    return <div>Hello! This is about Class-based component {this.a}</div>;
+    return  (
+      <>
+      <NavBar/>
+      <News/>
+      </>
+    )
   }
 }
