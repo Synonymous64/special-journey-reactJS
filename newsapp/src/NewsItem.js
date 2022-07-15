1eb44821d1d3b869e53173ef514a73d66cb8fe5f
Synonymous64 <prajwalurkude007@gmail.com>
2022-07-15 21:52:56 +0530

    Fetch API

--- newsapp/src/NewsItem.js
+++ newsapp/src/NewsItem.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let {title, description} = this.props
     return (
-      <div>This is a news Item</div>
-    )
+      <div>
+        <div className="card" style={{width: "18rem"}}>
+          <img src="https://ichef.bbci.co.uk/news/1024/branded_news/5623/production/_125915022_gettyimages-1307126058.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+            {description}
+            </p>
+            <a href="/newsDetails" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react'
-import NewsItem from '../NewsItem';
+import React, { Component } from "react";
+import NewsItem from "../NewsItem";
 export class News extends Component {
   render() {
     return (
-        <>
-      <div>This is a news component</div>
-        <NewsItem/>
-      </>
-
-    )
+      <div className="container my-3">
+        <h2>News Lit ~ Top HeadLines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
