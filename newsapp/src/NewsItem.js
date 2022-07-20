e0377c607cc55f7432f0227b049830f2a01dc44f
Synonymous64 <prajwalurkude007@gmail.com>
2022-07-20 23:35:57 +0530

    Articles Filtering

--- newsapp/src/NewsItem.js
+++ newsapp/src/NewsItem.js
@@ -2,18 +2,18 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let {title, description} = this.props
+    let {title, description, imageUrl, newsURL} = this.props
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src="https://ichef.bbci.co.uk/news/1024/branded_news/5623/production/_125915022_gettyimages-1307126058.jpg" className="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
             {description}
             </p>
-            <a href="/newsDetails" className="btn btn-primary">
-              Go somewhere
+            <a href="/newsDetails" className="btn btn-sm btn-primary">
+              Read More
             </a>
           </div>
         </div>
