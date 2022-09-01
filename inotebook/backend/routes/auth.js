b71393292f857f6fb9abae51fd708a261f676da4
Synonymous64 <prajwalurkude007@gmail.com>
2022-09-01 18:32:41 +0530

    Adding data validation using express -validator

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
-
-//* Create a user using: POST "/api/auth". Doesn't require Auth [Doesn't require auth]
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+const { body, validationResult } = require("express-validator");
+//* Create a user using: POST "/api/auth". Doesn't require Auth
+router.post(
+  "/",
+  [
+    body("name", "enter a valid name").isLength({ min: 3 }),
+    body("email", "enter a valid email").isEmail(),
+    body("password", "enter a valid password").isLength({ min: 5 }),
+    body("bio", "enter a valid bio").isLength({ max: 120 }),
+  ],
+  (req, res) => {
+    // console.log(req.body);
+    // const user = User(req.body);
+    // user.save();
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      bio: req.body.bio,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: "please enter the unique email",
+          message: err.message,
+        });
+      });
+  }
+);
 module.exports = router;
