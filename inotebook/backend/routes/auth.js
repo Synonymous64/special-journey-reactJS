15e56b73144e98230080e38d062ec5a6a7299527
Synonymous64 <prajwalurkude007@gmail.com>
2022-09-06 23:48:20 +0530

    Hashing/Salting Password + Json web token creation

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+//! Creating a token
+const JWT_SECRET = "Prajwalis$OOL";
 //* Create a user using: POST "/api/auth". Doesn't require Auth
 router.post(
   "/",
@@ -28,13 +32,24 @@ router.post(
           .status(400)
           .json({ error: "Sorry :/ A user with this email already exists" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      //* Create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
         bio: req.body.bio,
       });
-      res.json(user);
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      // console.log(authToken);
+      // res.json(user);
+      res.json({authToken});
     } catch (error) {
       console.log(error.message);
       res.status(500).json({ error: "Something went wrong :/ " });
