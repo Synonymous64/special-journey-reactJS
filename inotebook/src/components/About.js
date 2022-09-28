550e12c089996e547b692b493a03caa3f1056c01
Synonymous64 <prajwalurkude007@gmail.com>
2022-09-29 00:05:29 +0530

    UseContext API

--- inotebook/src/components/About.js
+++ inotebook/src/components/About.js
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import NoteContext from "../context/notes/NoteContext";
+import { useEffect } from "react";
 const About = () => {
+  const a = useContext(NoteContext);
+  useEffect(() => {
+    return () => {
+      a.update();
+    };
+    // eslint-disable-next-line
+  }, []);
   return (
     <div>
-      This is about
+      {/* using .state coz it is an object */}
+      This is about {a.state.name} and he is in {a.state.year} year
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
---
+++ inotebook/src/context/notes/NoteContext.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+//! Syntax for context creation
+const NoteContext = createContext();
+export default NoteContext;
