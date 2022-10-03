6570e7e8ef435632852ab56da92e696bb5f62921
Synonymous64 <prajwalurkude007@gmail.com>
2022-10-03 23:21:11 +0530

    Add Note component created

--- inotebook/src/components/AddNote.js
+++ inotebook/src/components/AddNote.js
@@ -4,7 +4,11 @@ import { useContext, useState } from "react";
 const AddNote = () => {
   const context = useContext(NoteContext);
   const { addNote } = context;
-  const [note, setNote] = useState({ title: "", description: "", tag: "default" });
+  const [note, setNote] = useState({
+    title: "",
+    description: "",
+    tag: "default",
+  });
   const handleClick = (e) => {
     e.preventDefault();
     addNote(note.title, note.description, note.tag);
