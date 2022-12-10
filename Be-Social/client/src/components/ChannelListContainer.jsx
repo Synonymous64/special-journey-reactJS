97b8c50e0653e23326190d10e0883f87dd09b41b
Synonymous64 <prajwalurkude007@gmail.com>
2022-12-10 22:25:00 +0530

    Components Created

--- Be-Social/client/src/components/ChannelListContainer.jsx
+++ Be-Social/client/src/components/ChannelListContainer.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
-
+import { SlSocialBehance } from "react-icons/sl";
+import { BiLogOut } from 'react-icons/bi'
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './'
+import Cookies from 'universal-cookie';
+import HostpitalIcon from '../assets/hospital.png'
+import LogOutIcon from '../assets/logout.png'
+const SideBar = () => (
+    <div className="channel-list__sidebar">
+        <div className='channel-list__sidebar__icon1'>
+            <div className='icon1__inner'>
+                {/* <img src={HostpitalIcon} alt="socialIcon" width="30" /> */}
+                <SlSocialBehance />
+            </div>
+        </div>
+        <div className='channel-list__sidebar__icon2'>
+            <div className='icon1__inner'>
+                {/* <img src={LogOutIcon} alt="socialIcon" width="30" /> */}
+                <BiLogOut />
+            </div>
+        </div>
+    </div>
+)
+const CompanyHeader = () => (
+    <div className='channel-list__header'>
+        <p className='channel-list__header__text'>BE-SOCIAL</p>
+    </div>
+)
 const ChannelListContainer = () => {
     return (
-        <div>
-            ChannelListContainer
-        </div>
+        <>
+            <SideBar />
+            <div className='channel-list__list__wrapper'>
+                <CompanyHeader />
+                <ChannelSearch />
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => { }}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            {...listProps}
+                            type='team'
+                        />
+                    )}
+                />
+            </div>
+        </>
     )
 }
 
---
+++ Be-Social/client/src/components/TeamChannelList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { AddChannel } from '../assets'
+const TeamChannelList = ({ children, error = false, loading, type }) => {
+    if (error) return type === 'team' ? (
+        <div className='team-channel-list'>
+            <p className='team-channel-list__message'>
+                Connection error, please wait for a moment or try later.
+            </p>
+        </div>
+    ) : null
+    if (loading) {
+        return (
+            <div className='team-channel-list'>
+                <p className='team-channel-list__message loading'>
+                    {type === 'team' ? 'Channels' : 'Message'} loading...
+                </p>
+            </div>
+        )
+    }
+    return (
+        <div className='team-channel-list'>
+            <div className='team-channel-list__header'>
+                <p className='team-channel-list__header__title'>
+                    {type === 'team' ? 'Channels' : 'Direct Message'}
+                </p>
+                {/* Button - add Channel */}
+            </div>
+            {children}
+        </div>
+    )
+}
+
+export default TeamChannelList
